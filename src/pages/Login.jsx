d3f4a07c04d3e5ceca25cb020b134d10426fcc2b
Rodrigo Freitas <kdigo.chr@gmail.com>
2022-06-20 20:44:35 -0300

    Feat: verificação de email e senha

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [enterBtn, setEnterBtn] = useState(true);
+
+  useEffect(() => {
+    const isDisabled = () => {
+      const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+      const MIN_PW_LENGTH = 6;
+      if (regex.test(email) && password.length > MIN_PW_LENGTH) {
+        setEnterBtn(false);
+      } else {
+        setEnterBtn(true);
+      }
+    };
+    isDisabled();
+  }, [email, password]);
+
   return (
     <>
       <h1>Hello world!</h1>
       <input
         data-testid="email-input"
         type="email"
+        onChange={ ({ target }) => setEmail(target.value) }
       />
       <input
         data-testid="password-input"
         type="password"
+        onChange={ ({ target }) => setPassword(target.value) }
       />
 
       <button
         data-testid="login-submit-btn"
         type="button"
+        disabled={ enterBtn }
       >
         Enter
       </button>
