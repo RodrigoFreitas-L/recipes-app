99a695f6b75b08652d2a33c842152452db38d95a
Rodrigo Freitas <kdigo.chr@gmail.com>
2022-06-22 17:10:52 -0300

    Feat: A requisição à API redireciona o usuário à página de detalhes da receita caso haja somente 1 match

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import { setDrinks } from '../redux/reducers/drinksSlice';
 
 function Drinks() {
+  const { drinks } = useSelector((state) => state.drinks);
+  const [filteredDrink, setFilteredDrink] = useState([]);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    setFilteredDrink(drinks);
+    if (filteredDrink.length === 1) {
+      history.push(`/drinks/${filteredDrink[0].idDrink}`);
+      setFilteredDrink([]);
+      dispatch(setDrinks([]));
+    }
+  }, [dispatch, filteredDrink, drinks, history]);
+
   return (
-    <Header title="Drinks" />
+    <>
+      <Header title="Drinks" />
+      { drinks.length !== 0 && filteredDrink.map((drink) => (
+        <div key={ drink.idDrink }>
+          <h1>{ drink.strDrink }</h1>
+        </div>
+      )) }
+    </>
   );
 }
-
 export default Drinks;
+
+// import React from 'react';
+// import Header from '../components/Header';
+
+// const Drinks = () => (
+//   <div>
+//     <Header title="Drinks" />
+//   </div>
+// );
+
+// export default Drinks;
