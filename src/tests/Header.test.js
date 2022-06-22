8ca5ecc00a4a0eb72ce66a692ef05b4e919beba8
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-06-22 02:20:13 -0300

    fix: erros do lint

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -15,194 +15,192 @@ const profileButtonTestId = 'profile-top-btn';
 const pageTitleTestId = 'page-title';
 const searchButtonTestId = 'search-top-btn';
 
-describe('Testes do componente Header', () => {
-  describe('9 - Elementos do header na tela principal de receitas', () => {
-    test('O botão de perfil, o título e o botão de search são renderizados', () => {
-      renderWithRouterAndRedux(<Foods />);
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      const pageTitle = screen.getByTestId(pageTitleTestId);
-      const searchButton = screen.getByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle && searchButton).toBeInTheDocument();
-    });
+describe('09 - Elementos do header na tela principal de receitas', () => {
+  test('O botão de perfil, o título e o botão de search são renderizados', () => {
+    renderWithRouterAndRedux(<Foods />);
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    const pageTitle = screen.getByTestId(pageTitleTestId);
+    const searchButton = screen.getByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle && searchButton).toBeInTheDocument();
+  });
+});
+
+describe('10 - O header aparece nas rotas corretas', () => {
+  test('Não tem header na tela de login', () => {
+    renderWithRouterAndRedux(<Login />);
+    const profileButton = screen.queryByTestId(profileButtonTestId);
+    const pageTitle = screen.queryByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle && searchButton).not.toBeInTheDocument();
   });
 
-  describe('10 - O header aparece nas rotas corretas', () => {
-    test('Não tem header na tela de login', () => {
-      renderWithRouterAndRedux(<Login />);
-      const profileButton = screen.queryByTestId(profileButtonTestId);
-      const pageTitle = screen.queryByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle && searchButton).not.toBeInTheDocument();
-    });
+  test('Header com todos os ícones na tela de principal de receitas de comidas', () => {
+    renderWithRouterAndRedux(<Foods />);
+    const pageTitle = screen.getByTestId(pageTitleTestId);
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    const searchButton = screen.getByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle && searchButton).toBeInTheDocument();
+  });
 
-    test('Header com todos os ícones na tela de principal de receitas de comidas', () => {
-      renderWithRouterAndRedux(<Foods />);
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      const pageTitle = screen.getByTestId(pageTitleTestId);
-      const searchButton = screen.getByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle && searchButton).toBeInTheDocument();
-    });
+  test('Header com todos os ícones na tela de principal de receitas de bebidas', () => {
+    renderWithRouterAndRedux(<Drinks />);
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    const pageTitle = screen.getByTestId(pageTitleTestId);
+    const searchButton = screen.getByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle && searchButton).toBeInTheDocument();
+  });
 
-    test('Header com todos os ícones na tela de principal de receitas de bebidas', () => {
-      renderWithRouterAndRedux(<Drinks />);
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      const pageTitle = screen.getByTestId(pageTitleTestId);
-      const searchButton = screen.getByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle && searchButton).toBeInTheDocument();
-    });
+  test('Não tem header na tela de detalhes de uma receita de comida', () => {
+    renderWithRouterAndRedux(<CardFood />);
+    const profileButton = screen.queryByTestId(profileButtonTestId);
+    const pageTitle = screen.queryByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle && searchButton).not.toBeInTheDocument();
+  });
 
-    test('Não tem header na tela de detalhes de uma receita de comida', () => {
-      renderWithRouterAndRedux(<CardFood />);
-      const profileButton = screen.queryByTestId(profileButtonTestId);
-      const pageTitle = screen.queryByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle && searchButton).not.toBeInTheDocument();
-    });
+  test('Não tem header na tela de detalhes de uma receita de bebida', () => {
+    renderWithRouterAndRedux(<CardDrink />);
+    const profileButton = screen.queryByTestId(profileButtonTestId);
+    const pageTitle = screen.queryByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle && searchButton).not.toBeInTheDocument();
+  });
 
-    test('Não tem header na tela de detalhes de uma receita de bebida', () => {
-      renderWithRouterAndRedux(<CardDrink />);
-      const profileButton = screen.queryByTestId(profileButtonTestId);
-      const pageTitle = screen.queryByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle && searchButton).not.toBeInTheDocument();
-    });
+  test('Não tem header na tela de receita em progresso de comida', () => {
+    renderWithRouterAndRedux(<CardFoodInProgress />);
+    const profileButton = screen.queryByTestId(profileButtonTestId);
+    const pageTitle = screen.queryByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle && searchButton).not.toBeInTheDocument();
+  });
 
-    test('Não tem header na tela de receita em progresso de comida', () => {
-      renderWithRouterAndRedux(<CardFoodInProgress />);
-      const profileButton = screen.queryByTestId(profileButtonTestId);
-      const pageTitle = screen.queryByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle && searchButton).not.toBeInTheDocument();
-    });
+  test('Não tem header na tela de receita em progresso de bebida', () => {
+    renderWithRouterAndRedux(<CardDrinkInProgress />);
+    const profileButton = screen.queryByTestId(profileButtonTestId);
+    const pageTitle = screen.queryByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle && searchButton).not.toBeInTheDocument();
+  });
 
-    test('Não tem header na tela de receita em progresso de bebida', () => {
-      renderWithRouterAndRedux(<CardDrinkInProgress />);
-      const profileButton = screen.queryByTestId(profileButtonTestId);
-      const pageTitle = screen.queryByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle && searchButton).not.toBeInTheDocument();
-    });
+  test('Header somente com profile e título na tela de explorar', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    history.push('/explore');
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    const pageTitle = screen.getByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle).toBeInTheDocument();
+    expect(searchButton).not.toBeInTheDocument();
+  });
 
-    test('Header somente com profile e título na tela de explorar', () => {
-      const { history } = renderWithRouterAndRedux(<App />);
-      history.push('/explore');
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      const pageTitle = screen.getByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle).toBeInTheDocument();
-      expect(searchButton).not.toBeInTheDocument();
-    });
+  test('Header somente com profile e título na tela de explorar comidas', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    history.push('/explore/foods');
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    const pageTitle = screen.getByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle).toBeInTheDocument();
+    expect(searchButton).not.toBeInTheDocument();
+  });
 
-    test('Header somente com profile e título na tela de explorar comidas', () => {
-      const { history } = renderWithRouterAndRedux(<App />);
-      history.push('/explore/foods');
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      const pageTitle = screen.getByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle).toBeInTheDocument();
-      expect(searchButton).not.toBeInTheDocument();
-    });
+  test('Header somente com profile e título na tela de explorar bebidas', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    history.push('/explore/drinks');
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    const pageTitle = screen.getByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle).toBeInTheDocument();
+    expect(searchButton).not.toBeInTheDocument();
+  });
 
-    test('Header somente com profile e título na tela de explorar bebidas', () => {
-      const { history } = renderWithRouterAndRedux(<App />);
-      history.push('/explore/drinks');
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      const pageTitle = screen.getByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle).toBeInTheDocument();
-      expect(searchButton).not.toBeInTheDocument();
-    });
-
-    test(`Header somente com profile e título na tela de explorar comidas por
+  test(`Header somente com profile e título na tela de explorar comidas por
     ingredientes`, () => {
-      const { history } = renderWithRouterAndRedux(<App />);
-      history.push('/explore/foods/ingredients');
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      const pageTitle = screen.getByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle).toBeInTheDocument();
-      expect(searchButton).not.toBeInTheDocument();
-    });
+    const { history } = renderWithRouterAndRedux(<App />);
+    history.push('/explore/foods/ingredients');
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    const pageTitle = screen.getByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle).toBeInTheDocument();
+    expect(searchButton).not.toBeInTheDocument();
+  });
 
-    test(`Header somente com profile e título na tela de explorar bebidas por
+  test(`Header somente com profile e título na tela de explorar bebidas por
     ingredientes`, () => {
-      const { history } = renderWithRouterAndRedux(<App />);
-      history.push('/explore/drinks/ingredients');
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      const pageTitle = screen.getByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle).toBeInTheDocument();
-      expect(searchButton).not.toBeInTheDocument();
-    });
+    const { history } = renderWithRouterAndRedux(<App />);
+    history.push('/explore/drinks/ingredients');
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    const pageTitle = screen.getByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle).toBeInTheDocument();
+    expect(searchButton).not.toBeInTheDocument();
+  });
 
-    test(`Header com todos os ícones na tela de explorar comidas por
+  test(`Header com todos os ícones na tela de explorar comidas por
      nacionalidade`, () => {
-      const { history } = renderWithRouterAndRedux(<App />);
-      history.push('/explore/foods/nationalities');
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      const pageTitle = screen.getByTestId(pageTitleTestId);
-      const searchButton = screen.getByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle && searchButton).toBeInTheDocument();
-    });
-
-    test('Header somente com profile e título na tela de perfil', () => {
-      const { history } = renderWithRouterAndRedux(<App />);
-      history.push('/profile');
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      const pageTitle = screen.getByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle).toBeInTheDocument();
-      expect(searchButton).not.toBeInTheDocument();
-    });
-
-    test('Header somente com profile e título na tela de receitas feitas', () => {
-      const { history } = renderWithRouterAndRedux(<App />);
-      history.push('/done-recipes');
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      const pageTitle = screen.getByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle).toBeInTheDocument();
-      expect(searchButton).not.toBeInTheDocument();
-    });
-
-    test('Header somente com profile e título na tela de receitas favoritas', () => {
-      const { history } = renderWithRouterAndRedux(<App />);
-      history.push('/favorite-recipes');
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      const pageTitle = screen.getByTestId(pageTitleTestId);
-      const searchButton = screen.queryByTestId(searchButtonTestId);
-      expect(profileButton && pageTitle).toBeInTheDocument();
-      expect(searchButton).not.toBeInTheDocument();
-    });
+    const { history } = renderWithRouterAndRedux(<App />);
+    history.push('/explore/foods/nationalities');
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    const pageTitle = screen.getByTestId(pageTitleTestId);
+    const searchButton = screen.getByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle && searchButton).toBeInTheDocument();
   });
 
-  describe('11 - Redireciona para a tela de perfil ao clicar no botão de perfil', () => {
-    test('A mudança de tela ocorre corretamente', () => {
-      const { history } = renderWithRouterAndRedux(<Foods />);
-      expect(history.location.pathname).toBe('/');
-      const profileButton = screen.getByTestId(profileButtonTestId);
-      userEvent.click(profileButton);
-      expect(history.location.pathname).toBe('/profile');
-    });
+  test('Header somente com profile e título na tela de perfil', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    history.push('/profile');
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    const pageTitle = screen.getByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle).toBeInTheDocument();
+    expect(searchButton).not.toBeInTheDocument();
   });
 
-  describe('12 - Mostrar/esconder barra de busca ao clicar no botão', () => {
-    test('Ao clicar no botão de busca pela primeira vez a barra aparece', () => {
-      renderWithRouterAndRedux(<Foods />);
-      const searchButton = screen.getByTestId(searchButtonTestId);
-      userEvent.click(searchButton);
-      const searchInput = screen.getByTestId('search-input');
-      expect(searchInput).toBeInTheDocument();
-    });
+  test('Header somente com profile e título na tela de receitas feitas', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    history.push('/done-recipes');
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    const pageTitle = screen.getByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle).toBeInTheDocument();
+    expect(searchButton).not.toBeInTheDocument();
+  });
 
-    test('Ao clicar no botão de busca pela segunda vez a barra desaparece', () => {
-      renderWithRouterAndRedux(<Foods />);
-      const searchButton = screen.getByTestId(searchButtonTestId);
-      userEvent.click(searchButton);
-      const searchInput = screen.getByTestId('search-input');
-      expect(searchInput).toBeInTheDocument();
-      userEvent.click(searchButton);
-      expect(searchInput).not.toBeInTheDocument();
-    });
+  test('Header somente com profile e título na tela de receitas favoritas', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    history.push('/favorite-recipes');
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    const pageTitle = screen.getByTestId(pageTitleTestId);
+    const searchButton = screen.queryByTestId(searchButtonTestId);
+    expect(profileButton && pageTitle).toBeInTheDocument();
+    expect(searchButton).not.toBeInTheDocument();
+  });
+});
+
+describe('11 - Redireciona para a tela de perfil ao clicar no botão de perfil', () => {
+  test('A mudança de tela ocorre corretamente', () => {
+    const { history } = renderWithRouterAndRedux(<Foods />);
+    expect(history.location.pathname).toBe('/');
+    const profileButton = screen.getByTestId(profileButtonTestId);
+    userEvent.click(profileButton);
+    expect(history.location.pathname).toBe('/profile');
+  });
+});
+
+describe('12 - Mostrar/esconder barra de busca ao clicar no botão', () => {
+  test('Ao clicar no botão de busca pela primeira vez a barra aparece', () => {
+    renderWithRouterAndRedux(<Foods />);
+    const searchButton = screen.getByTestId(searchButtonTestId);
+    userEvent.click(searchButton);
+    const searchInput = screen.getByTestId('search-input');
+    expect(searchInput).toBeInTheDocument();
+  });
+
+  test('Ao clicar no botão de busca pela segunda vez a barra desaparece', () => {
+    renderWithRouterAndRedux(<Foods />);
+    const searchButton = screen.getByTestId(searchButtonTestId);
+    userEvent.click(searchButton);
+    const searchInput = screen.getByTestId('search-input');
+    expect(searchInput).toBeInTheDocument();
+    userEvent.click(searchButton);
+    expect(searchInput).not.toBeInTheDocument();
   });
 });
