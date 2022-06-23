2b5d9d3a8b1557a4c428faa7478102c9fb595054
AndreAraujo1998 <andre.florencio98@gmail.com>
2022-06-23 16:57:12 -0300

    Feat: Componentes Explore Foods e Drinks implementados

--- src/components/ExploreDrinks.jsx
+++ src/components/ExploreDrinks.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -6,6 +7,30 @@ function ExploreDrinks() {
   return (
     <>
       <Header title="Explore Drinks" />
+      <div className="ExploreCards">
+        <div>
+          <div
+            className="ExploreCard"
+          >
+            <Link
+              to="/explore/drinks/ingredients"
+              data-testid="explore-by-ingredient"
+            >
+              By Ingredient
+            </Link>
+          </div>
+          <div
+            className="ExploreCard"
+          >
+            <Link
+              to="/explore/foods/" // TODO
+              data-testid="explore-surprise"
+            >
+              Surprise me!
+            </Link>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
--- src/components/ExploreFoods.jsx
+++ src/components/ExploreFoods.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
+import '../styles/Explore.css';
 
 function ExploreFoods() {
   return (
     <>
       <Header title="Explore Foods" />
+      <div className="ExploreCards">
+        <div>
+          <div
+            className="ExploreCard"
+          >
+            <Link
+              to="/explore/foods/ingredients"
+              data-testid="explore-by-ingredient"
+            >
+              By Ingredient
+            </Link>
+          </div>
+          <div
+            className="ExploreCard"
+          >
+            <Link
+              to="/explore/foods/nationalities"
+              data-testid="explore-by-nationality"
+            >
+              By Nationality
+            </Link>
+          </div>
+          <div
+            className="ExploreCard"
+          >
+            <Link
+              to="/explore/foods/" // TODO
+              data-testid="explore-surprise"
+            >
+              Surprise me!
+            </Link>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
