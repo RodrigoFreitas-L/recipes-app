31d0f795b5deb313987d335cf9eb0eb623d349ad
AndreAraujo1998 <andre.florencio98@gmail.com>
2022-06-23 17:38:26 -0300

    Feat: surprise me implementado para explore foods and drinks

--- src/components/ExploreDrinks.jsx
+++ src/components/ExploreDrinks.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
 function ExploreDrinks() {
+  const history = useHistory();
+  const onClickSurprise = async () => {
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+    const response = await fetch(url);
+    const { drinks } = await response.json();
+    history.push(`/drinks/${drinks[0].idDrink}`);
+  };
+
   return (
     <>
       <Header title="Explore Drinks" />
@@ -22,12 +30,13 @@ function ExploreDrinks() {
           <div
             className="ExploreCard"
           >
-            <Link
-              to="/explore/foods/" // TODO
+            <button
+              type="button"
               data-testid="explore-surprise"
+              onClick={ () => onClickSurprise() }
             >
               Surprise me!
-            </Link>
+            </button>
           </div>
         </div>
       </div>
--- src/components/ExploreFoods.jsx
+++ src/components/ExploreFoods.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
 import '../styles/Explore.css';
 
 function ExploreFoods() {
+  const history = useHistory();
+  const onClickSurprise = async () => {
+    const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    const response = await fetch(url);
+    const { meals } = await response.json();
+    history.push(`/foods/${meals[0].idMeal}`);
+  };
+
   return (
     <>
       <Header title="Explore Foods" />
@@ -33,12 +41,13 @@ function ExploreFoods() {
           <div
             className="ExploreCard"
           >
-            <Link
-              to="/explore/foods/" // TODO
+            <button
+              type="button"
               data-testid="explore-surprise"
+              onClick={ () => onClickSurprise() }
             >
               Surprise me!
-            </Link>
+            </button>
           </div>
         </div>
       </div>
