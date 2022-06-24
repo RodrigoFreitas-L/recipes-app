a80601586f5a5ddb5c40eacfbb5d1fc2da1fd3fe
Maykon Freitas <maykon17programmer@gmail.com>
2022-06-24 16:57:32 -0300

    feat: Finalizando os requisitos 33, 34, 35, 36 do projeto

--- src/pages/CardDrink.jsx
+++ src/pages/CardDrink.jsx
@@ -1,15 +1,75 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+// Componentes
+import Ingredients from '../components/RecipeDetails/Ingredients';
+import BoxRecomendation from '../components/RecipeDetails/BoxRecomendation';
+
+import { setDrinks } from '../redux/reducers/drinksSlice';
+import { setFoods } from '../redux/reducers/foodsSlice';
+
 function CardDrink() {
+  const dispatch = useDispatch();
+  const { drinks } = useSelector((state) => state.drinks);
+  const { foods } = useSelector((state) => state.foods);
   const { location } = useHistory();
-  const [idDrink, setIdDrink] = useState('');
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
+    // Pegando o ID da URL
     const id = location.pathname.split('/')[2];
-    setIdDrink(id);
-  }, [location]);
+    // Fazendo a requisição a API pelo ID da receita
+    const fetchFoods = async () => {
+      const MAX_DRINKS_LIST = 7;
+      const endpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+      const response = await fetch(endpoint);
+      const data = await response.json();
+      dispatch(setFoods(data.meals.slice(1, MAX_DRINKS_LIST)));
+    };
+
+    const fetchDrinks = async () => {
+      const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+      const endpoint = `${url}${id}`;
+      const response = await fetch(endpoint);
+      const data = await response.json();
+      dispatch(setDrinks(data.drinks));
+      fetchFoods();
+      setLoading(false);
+    };
+    fetchDrinks();
+  }, [location, loading, dispatch]);
+
+  const renderRecipeDetails = () => {
+    const details = drinks.map((drink) => {
+      console.log(drink);
+      const { idDrink } = drink;
+      return (
+        <div key={ idDrink }>
+          <img
+            data-testid="recipe-photo"
+            src={ drink.strDrinkThumb }
+            alt={ drink.strDrink }
+          />
+          <p data-testid="recipe-title">{ drink.strDrink }</p>
+          <button type="button" data-testid="share-btn">Share</button>
+          <button type="button" data-testid="favorite-btn">Favorite</button>
+          <p data-testid="recipe-category">{ drink.strAlcoholic }</p>
+          <Ingredients recipe={ drink } />
+          <p data-testid="instructions">{ drink.strInstructions }</p>
+          <BoxRecomendation recomendations={ foods } />
+          <button type="button" data-testid="start-recipe-btn">Start Recipe</button>
+        </div>
+      );
+    });
+    return details;
+  };
+
   return (
-    <h2>{`Hello World - ${idDrink}`}</h2>
+    <div>
+      { loading && <h2>loading ...</h2> }
+      { !loading && renderRecipeDetails() }
+    </div>
   );
 }
 
